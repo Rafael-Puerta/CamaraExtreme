355dbacc9ded92ffde98b016ef35aa0fbd044752
Rafael-Puerta <rafapc1992@gmail.com>
2022-12-19 16:29:00 +0100

    Initial commit

---
+++ app/src/main/java/com/example/camaraextreme/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.camaraextreme;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    public ActivityResultLauncher<Intent> someActivityResultLauncher ;
+    public static int RC_PHOTO_PICKER = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        this.someActivityResultLauncher= registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            // There are no request codes
+                            Intent data = result.getData();
+                            Uri uri = data.getData();
+                            ImageView imageView = findViewById(R.id.imageView);
+                            imageView.setImageURI(uri);
+                        }
+                    }
+                });
+        Button b=findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSomeActivityForResult(null);
+            }
+        });
+    }
+
+    public void openSomeActivityForResult(View view) {
+
+
+        //Create Intent
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/jpg");
+        intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+        //Launch activity to get result
+        someActivityResultLauncher.launch(intent);
+    }
+
+}
