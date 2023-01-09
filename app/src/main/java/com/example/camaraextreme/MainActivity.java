e32377116790763b8f712a419b879d732fe21305
Rafael-Puerta <rafapc1992@gmail.com>
2023-01-09 16:38:33 +0100

    v2

--- app/src/main/java/com/example/camaraextreme/MainActivity.java
+++ app/src/main/java/com/example/camaraextreme/MainActivity.java
@@ -8,14 +8,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     public ActivityResultLauncher<Intent> someActivityResultLauncher ;
+    public ActivityResultLauncher<Intent> someActivityResultLauncher2 ;
     public static int RC_PHOTO_PICKER = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +38,21 @@ public class MainActivity extends AppCompatActivity {
                         }
                     }
                 });
+        this.someActivityResultLauncher2= registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+
+                        if ( result.getResultCode() == RESULT_OK) {
+                            Bundle extras = result.getData().getExtras();
+                            Bitmap imageBitmap = (Bitmap) extras.get("data");
+                            ImageView imageView = findViewById(R.id.imageView);
+                            imageView.setImageBitmap(imageBitmap);
+                        }
+
+                    }
+                });
         Button b=findViewById(R.id.button);
         b.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,10 +60,16 @@ public class MainActivity extends AppCompatActivity {
                 openSomeActivityForResult(null);
             }
         });
+        Button b2=findViewById(R.id.button2);
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSomeActivityForResult2(null);
+            }
+        });
     }
 
     public void openSomeActivityForResult(View view) {
-
 
         //Create Intent
         Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
@@ -53,6 +77,12 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
         //Launch activity to get result
         someActivityResultLauncher.launch(intent);
+    }
+    public void openSomeActivityForResult2(View view) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        someActivityResultLauncher2.launch(takePictureIntent);
+
     }
 
 }
