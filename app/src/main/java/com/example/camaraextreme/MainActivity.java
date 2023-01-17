598194d9b092dff542bc1ef669ecc0be674ec2b2
Rafael-Puerta <rafapc1992@gmail.com>
2023-01-17 17:30:47 +0100

    v3

--- app/src/main/java/com/example/camaraextreme/MainActivity.java
+++ app/src/main/java/com/example/camaraextreme/MainActivity.java
@@ -5,21 +5,30 @@ import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityOptionsCompat;
+import androidx.core.content.FileProvider;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
     public ActivityResultLauncher<Intent> someActivityResultLauncher ;
     public ActivityResultLauncher<Intent> someActivityResultLauncher2 ;
     public static int RC_PHOTO_PICKER = 0;
+    public Uri photoURI;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,10 +54,10 @@ public class MainActivity extends AppCompatActivity {
                     public void onActivityResult(ActivityResult result) {
 
                         if ( result.getResultCode() == RESULT_OK) {
-                            Bundle extras = result.getData().getExtras();
-                            Bitmap imageBitmap = (Bitmap) extras.get("data");
+                            //Bundle extras = result.getData().getExtras();
+                            //Bitmap imageBitmap = (Bitmap) extras.get("data");
                             ImageView imageView = findViewById(R.id.imageView);
-                            imageView.setImageBitmap(imageBitmap);
+                            imageView.setImageURI(photoURI);
                         }
 
                     }
@@ -81,8 +90,43 @@ public class MainActivity extends AppCompatActivity {
     public void openSomeActivityForResult2(View view) {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
-        someActivityResultLauncher2.launch(takePictureIntent);
+
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                photoURI = FileProvider.getUriForFile(this,
+                        "com.example.camaraextreme.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+            }
+            someActivityResultLauncher2.launch(takePictureIntent );
+
+
 
     }
+    String currentPhotoPath;
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
 
 }
